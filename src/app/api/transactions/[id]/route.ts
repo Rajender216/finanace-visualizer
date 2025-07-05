795ee0a7ac4b5704da9ea4f0eff3d3a1e04c1732
Rajender216 <216rajender@gmail.com>
2025-07-05 14:05:25 +0530

    delete fixed

--- src/app/api/transactions/[id]/route.ts
+++ src/app/api/transactions/[id]/route.ts
@@ -1,13 +1,17 @@
-// src/app/api/transactions/[id]/route.ts
-
 import { connectDB } from '@/lib/db'
 import { Transaction } from '@/models/transaction'
 import { NextResponse } from 'next/server'
 
-export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  req: Request,
+  context: { params: { id: string } }
+) {
   try {
     await connectDB()
-    await Transaction.findByIdAndDelete(params.id)
+    const { id } = context.params
+
+    await Transaction.findByIdAndDelete(id)
+
     return NextResponse.json({ message: 'Transaction deleted successfully' })
   } catch (err) {
     console.error('DELETE error:', err)
